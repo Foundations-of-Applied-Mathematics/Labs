0229fa58186504e86e16769e0fc02eb730bd1b5c
rdorff <beccadorff@gmail.com>
2020-01-13 11:10:23 -0700

    Added untracked files

---
+++ DataScienceEssentials/MPI/pi.py
@@ -0,0 +1,14 @@
+# pi.py
+import numpy as np
+from scipy import linalg as la
+
+
+# Get 2000 random points in the 2-D domain [-1,1]x[-1,1].
+points = np.random.uniform(-1, 1, (2,2000))
+
+# Determine how many points are within the unit circle.
+lengths = la.norm(points, axis=0)
+num_within = np.count_nonzero(lengths < 1)
+
+# Estimate the circle's area.
+print(4 * (num_within / 2000))
