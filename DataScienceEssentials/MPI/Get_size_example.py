890bc169306dfc430714cee53f5db5a7ca56bc9d
pas1776 <pas1776@byu.local@acme29.byu.edu>
2022-08-13 14:29:40 -0600

    update 3 8-13-22

--- DataScienceEssentials/MPI/Get_size_example.py
+++
@@ -1,4 +0,0 @@
-#Get_size_example.py
-from mpi4py import MPI
-SIZE = MPI.COMM_WORLD.Get_size()
-print("The number of processes is {}.".format(SIZE))
--- DataScienceEssentials/MPI/hello.py
+++
@@ -1,7 +0,0 @@
-#hello.py
-from mpi4py import MPI
-
-COMM = MPI.COMM_WORLD
-RANK = COMM.Get_rank()
-
-print("Hello world! I'm process number {}.".format(RANK))
